c5c5b0fd0dc0cafd97e128136df77f461399747f
Aditya Deshmukh <adityadeshmukh14100@gmail.com>
2026-06-07 10:01:01 -0700

    Added Java basic logic problems

--- src/Numbers/DecimalToHexadecimal.java
+++ src/Numbers/DecimalToHexadecimal.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class DecimalToHexadecimal {
 	public static void main(String[] args) {
-		
+		 
 		// hexadecimal nos. -> 16 numbers [0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F]
 		
 		char ch[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
