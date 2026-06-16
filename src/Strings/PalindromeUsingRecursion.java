12aac4ac36ebe32a333b7fd2253ff19fa9129296
Aditya Deshmukh <adityadeshmukh14100@gmail.com>
2026-06-16 08:34:03 -0700

    Added Java basic logic problem

---
+++ src/Strings/PalindromeUsingRecursion.java
@@ -0,0 +1,22 @@
+package Strings;
+
+public class PalindromeUsingRecursion {
+	
+	public static boolean isPalin(String str, int i, int j) {
+		if (i>=j) {
+			return true;
+		}
+		
+		if (str.charAt(i)!=str.charAt(j)) {
+			return false;
+		}
+		return isPalin(str, i+1, j-1);
+	}
+	
+	public static void main(String[] args) {
+		
+		String str = "LeveL";
+		System.out.println(isPalin(str, 0, str.length()-1));
+	}
+
+}
